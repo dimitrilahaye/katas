00eb2bc2f3796c8d828067696ff9c45bca6802ff
Dimitri Lahaye <dimitrilahaye.dev@gmail.com>
2024-07-01 09:23:11 +0200

    test(coffee-machine): remove example test file

--- test/coffee-machine/example.test.ts
+++
@@ -1,9 +0,0 @@
-import {answer} from "../../src/coffee-machine";
-
-describe('Coffee Machine kata', () => {
-    describe('#answer', () => {
-        it('should return 42', () => {
-            expect(answer()).toBe(42);
-        });
-    });
-});
